28a3fb812284447152b60d312e499b1da702848c
vomolo <valentineomolo1@gmail.com>
2024-07-11 18:05:15 +0300

    solutions

--- pra/main.go
+++ pra/main.go
@@ -1,15 +1,24 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "fmt"
+
+func Max(a []int) int {
+	if len(a) < 1 {
+		return 0
+	}
+
+	maxVal := a[0]
+
+	for _, val := range a {
+		if val > maxVal {
+			maxVal = val
+		}
+	}
+	return maxVal
+}
 
 func main() {
-	arg := "hello"
-	run := []rune(arg)
-
-	z01.PrintRune(rune(run[len(run)-1]))
-
-	// fmt.Println(string(arg[5]))
-	z01.PrintRune('\n')
+	a := []int{123, 433, 435, 567, 879}
+	res := Max(a)
+	fmt.Println(res)
 }
