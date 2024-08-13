09cb0a6d2d846bb6ee5a4ce9cf3f1994f61676c8
vomolo <valentineomolo1@gmail.com>
2024-08-13 18:44:12 +0300

    splits

--- main.go
+++ main.go
@@ -5,20 +5,35 @@ import "fmt"
 func main() {
 	str := "    hello  good     morning   "
 	res := TrimSpace1(str)
-	fmt.Println(res)
+	res1 := WordSlice(res)
+	fmt.Println(res1)
 }
 
 func TrimSpace1(str string) string {
 	start := 0
-	end := len(str)
+	end := len(str) - 1
 
 	for start < end && str[start] == ' ' {
 		start++
 	}
 
-	for end > start && str[end-1] == ' ' {
+	for end > start && str[end] == ' ' {
 		end--
 	}
-
 	return str[start:end]
+}
+
+func WordSlice(str string) string {
+	var word []rune
+
+	for _, cha := range str {
+		if cha != ' ' {
+			word = append(word, cha)
+		}
+
+		if cha == ' ' {
+			fmt.Println()
+		}
+	}
+	return string(word)
 }
