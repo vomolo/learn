edbc43a6b95fbe3b3472f9be895b8744cbb22cbf
vomolo <valentineomolo1@gmail.com>
2024-10-31 14:44:09 +0300

    txt git.

--- solutions/fprime/main.go
+++ solutions/fprime/main.go
@@ -6,25 +6,25 @@ import (
 	"strconv"
 )
 
-func fprime(value int) {
-	if value == 1 {
-		return
-	}
-	divisionIterator := 2
-	for value > 1 {
-		if value%divisionIterator == 0 {
-			fmt.Print(divisionIterator)
-			value = value / divisionIterator
+// func fprime(value int) {
+// 	if value == 1 {
+// 		return
+// 	}
+// 	div := 2
+// 	for value > 1 {
+// 		if value%div == 0 {
+// 			fmt.Print(div)
+// 			value = value / div
 
-			if value > 1 {
-				fmt.Print("*")
-			}
-			divisionIterator--
-		}
-		divisionIterator++
-	}
-	fmt.Println()
-}
+// 			if value > 1 {
+// 				fmt.Print("*")
+// 			}
+// 			div--
+// 		}
+// 		div++
+// 	}
+// 	fmt.Println()
+// }
 
 func main() {
 	if len(os.Args) == 2 {
@@ -32,4 +32,24 @@ func main() {
 			fprime(i)
 		}
 	}
+}
+
+func fprime(val int) {
+	if val == 1 {
+		return
+	}
+
+	div := 2
+	for val > 1 {
+		if val%div == 0 {
+			fmt.Print(div)
+			val = val / div
+			if val > 1 {
+				fmt.Print("*")
+			}
+			div--
+		}
+		div++
+	}
+	fmt.Println()
 }
